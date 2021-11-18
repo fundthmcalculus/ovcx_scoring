320cf34a8e0788539c658c8c36117fa0fa32ac87
Scott Phillips <polygonguru@gmail.com>
2021-11-17 22:18:14 -0500

    Initial setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ovcx_scoring',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Scott Phillips',
+    author_email='polygonguru@gmail.com',
+    description='Scoring automation for OVCX'
+)
